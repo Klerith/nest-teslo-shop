8c0827e8b8f319d7f48ee1780313cbd6f36067e5
Fernando Herrera <fernando.herrera85@gmail.com>
2025-01-16 10:54:21 -0500

    Update README and enhance SeedController documentation

--- README.md
+++ README.md
@@ -2,27 +2,30 @@
   <a href="http://nestjs.com/" target="blank"><img src="https://nestjs.com/img/logo-small.svg" width="200" alt="Nest Logo" /></a>
 </p>
 
-
 # Teslo API
 
 1. Clonar proyecto
-2. ```yarn install```
-3. Clonar el archivo ```.env.template``` y renombrarlo a ```.env```
+2. `yarn install`
+3. Clonar el archivo `.env.template` y renombrarlo a `.env`
 4. Cambiar las variables de entorno
 5. Levantar la base de datos
+
 ```
 docker-compose up -d
 ```
 
-6. Levantar: ```yarn start:dev```
+6. Levantar:
 
-7. Ejecutar SEED 
+```
+npm run start:dev
+yarn start:dev
+
+```
+
+7. Ejecutar SEED
+
 ```
 http://localhost:3000/api/seed
 ```
 
-
-
 # Production notes:
-
-
--- src/seed/seed.controller.ts
+++ src/seed/seed.controller.ts
@@ -1,5 +1,5 @@
-import { Controller, Get, } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Controller, Get } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 import { ValidRoles } from '../auth/interfaces';
 import { Auth } from '../auth/decorators';
@@ -13,7 +13,13 @@ export class SeedController {
 
   @Get()
   // @Auth( ValidRoles.admin )
+  @ApiOperation({
+    summary: 'Destruye y crea una nueva base de datos',
+    description:
+      'Destruye y crea la base de datos de productos y usuarios, esto invalida también tokens existentes y usuarios creados.',
+  })
+  @ApiResponse({ status: 200, description: 'Seed ejecutado con éxito' })
   executeSeed() {
-    return this.seedService.runSeed()
+    return this.seedService.runSeed();
   }
 }
