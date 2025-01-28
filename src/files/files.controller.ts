a5a526dc6cacf9eb48c088a48ace2eb892fc487c
Fernando Herrera <fernando.herrera85@gmail.com>
2025-01-28 14:18:37 -0500

    Refactor FilesController for improved code readability and add fileName to upload response

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get, Post, Param, UploadedFile, UseInterceptors, BadRequestException, Res } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Param,
+  UploadedFile,
+  UseInterceptors,
+  BadRequestException,
+  Res,
+} from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { ApiTags } from '@nestjs/swagger';
 import { FileInterceptor } from '@nestjs/platform-express';
@@ -20,37 +29,34 @@ export class FilesController {
   @Get('product/:imageName')
   findProductImage(
     @Res() res: Response,
-    @Param('imageName') imageName: string
+    @Param('imageName') imageName: string,
   ) {
+    const path = this.filesService.getStaticProductImage(imageName);
 
-    const path = this.filesService.getStaticProductImage( imageName );
-
-    res.sendFile( path );
+    res.sendFile(path);
   }
 
-
-
   @Post('product')
-  @UseInterceptors( FileInterceptor('file', {
-    fileFilter: fileFilter,
-    // limits: { fileSize: 1000 }
-    storage: diskStorage({
-      destination: './static/products',
-      filename: fileNamer
-    })
-  }) )
-  uploadProductImage( 
-    @UploadedFile() file: Express.Multer.File,
-  ){
-
-    if ( !file ) {
+  @UseInterceptors(
+    FileInterceptor('file', {
+      fileFilter: fileFilter,
+      // limits: { fileSize: 1000 }
+      storage: diskStorage({
+        destination: './static/products',
+        filename: fileNamer,
+      }),
+    }),
+  )
+  uploadProductImage(@UploadedFile() file: Express.Multer.File) {
+    if (!file) {
       throw new BadRequestException('Make sure that the file is an image');
     }
 
     // const secureUrl = `${ file.filename }`;
-    const secureUrl = `${ this.configService.get('HOST_API') }/files/product/${ file.filename }`;
+    const secureUrl = `${this.configService.get('HOST_API')}/files/product/${
+      file.filename
+    }`;
 
-    return { secureUrl };
+    return { secureUrl, fileName: file.filename };
   }
-
 }
